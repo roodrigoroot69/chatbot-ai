e44d92cd28b1fb23c286dd3b1b0d554f514aaf3d
Rodrigo <fraurcino@hotmail.com>
2024-01-09 12:12:48 -0600

    feat: First commit

---
+++ src/chati/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from src.chati.models import Lessons, Keywords, Examples
+
+admin.site.register(Lessons)
+admin.site.register(Keywords)
+admin.site.register(Examples)
---
+++ src/chati/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+# Create your models here.
+
+
+class BaseModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Lessons(BaseModel):
+
+    title = models.CharField(max_length=250)
+    lesson = models.TextField()
+    publication_day = models.DateField()
+
+    def __str__(self) -> str:
+        return f'{self.title}'
+
+
+class Examples(BaseModel):
+
+    lesson = models.ForeignKey(Lessons, on_delete=models.CASCADE)
+    example = models.TextField()
+
+    def __str__(self) -> str:
+        return f'{self.lesson}'
+
+
+class Keywords(BaseModel):
+
+    keywords = models.JSONField()
+    lesson = models.ForeignKey(Lessons, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'{self.keywords}'
